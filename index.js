8cc797aa9a77b8d1128347d3762d0c36b83db911
BaurMus <musilimov_b@mail.ru>
2023-05-04 15:07:03 +0600

    install express-handlebars/create index.hbs/connect to mongodb

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log('Server has been started...');
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs',
 });
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+async function start() {
+  try {
+     await mongoose.connect('mongodb+srv://musilimovb:bake_Js1984@cluster1.ulim0rl.mongodb.net/todos', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+     });
+     app.listen(PORT, () => {
+      console.log('Server has been started...');
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start();
